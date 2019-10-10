dbbe2bb91d3fb3f896522e8763c92a179c10e70f
Srinivas Talnikar <shrinivastalnikar@gmail.com>
2019-10-10 23:07:37 +0000

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var Fuse = require('fuse.js');
+
+medData = JSON.parse(fs.readFileSync('./data/data.json').toString());
+medDataFuse = new Fuse(medData, {
+	keys: ['name', 'med_type', 'generic', 'manufacturer'],
+	shouldSort: true
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.json(medData);
+	// res.render('index', { data:  JSON.stringify(http_response)});
+});
+
+router.get('/search', function(req, res, next){
+	var result = medDataFuse.search(req.query.q);
+	res.json(result);
+})
+
+module.exports = router;
