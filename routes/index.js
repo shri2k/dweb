d87f354cdcf87656785648b01c59de43d53a75c0
Srinivas Talnikar <shrinivastalnikar@gmail.com>
2019-10-11 19:11:34 +0000

    Updated UI and stuff

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,20 @@ var router = express.Router();
 var fs = require('fs');
 var Fuse = require('fuse.js');
 
-medData = JSON.parse(fs.readFileSync('./data/data.json').toString());
+medData = JSON.parse(fs.readFileSync('./data/data_copy.json').toString());
 medDataFuse = new Fuse(medData, {
-	keys: ['name', 'med_type', 'generic', 'manufacturer'],
+	keys: [{
+		name: 'name',
+		weight: 0.5,
+	},
+	{
+		name: "med_type",
+		weight: 0.1
+	},
+	{
+		name: "generic",
+		weight: 0.4
+	}],
 	shouldSort: true
 });
 
@@ -13,6 +24,10 @@ medDataFuse = new Fuse(medData, {
 router.get('/', function(req, res, next) {
 	// res.json(medData);
 	res.render('index');
+});
+router.get('/lucky', function(req, res, next) {
+	// res.json(medData);
+	res.render('maps');
 });
 
 router.get('/search', function(req, res, next){
@@ -23,5 +38,6 @@ router.get('/search', function(req, res, next){
 	};
 	res.render('search', {data: data});
 })
+
 
 module.exports = router;
